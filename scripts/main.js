c0ebe62ffec621f3016439842209ff259df4e494
Travis Mendoza <travmendoz@gmail.com>
2024-04-24 18:46:43 -0400

    complete supercharging website

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,34 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/spanish-portrait.jpg") {
+        myImage.setAttribute("src", "images/bat-symbol.jpeg");
+    } else {
+        myImage.setAttribute("src", "images/spanish-portrait.jpg");
+    }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName)
+        myHeading.textContent = `Hello, ${myName}`;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Hello, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+};
